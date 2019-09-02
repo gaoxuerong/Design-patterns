ee463fb0e0cb760d711892ec195b98d9cdcc690f
gaoxuerong <gaoxuerong123@gmail.com>
2019-09-02 21:38:54 +0800

    增加缓存代理demo(阶乘)
    :q

--- proxy/缓存代理/1.js
+++ proxy/缓存代理/1.js
@@ -1,8 +1,15 @@
 // 1!=1;4!=3*2*1;n!= n*(n-1)!
-let m = function(n){
-    if(n<=1){
-        return 1;
-    }else{
-        return n*m(n-1)
-    }
+let m = function (n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * m(n - 1)
+  }
+}
+let sum = function (n) {
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result = result+m(i)
+  }
+  return result;
 }
